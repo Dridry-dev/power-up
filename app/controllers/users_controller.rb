3dea67d847da8f74903621f3fa177a9f14d2f413
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-21 23:15:40 +0100

    minor changes in seed and table prestation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_list, only: :show
 
   def index
     @users = User.all
@@ -12,5 +13,15 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :mail, :description, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+puts 'Database cleaned'
+
+random_id_hero = rand(1..731)
+url = "https://superheroapi.com/api/10229599919482949/"
+10.times do
+  puts "Importing 10 random heroes"
+  buddies = JSON.parse(URI.open("#{url}#{random_id_hero}").read)
+  buddies.each do |buddy|
+    puts "Creating #{buddy['name']}"
+    User.create(
+      name: buddy['name'],
+      description: buddy['connections'].first,
+      photo: photo.attach(io: buddy['image'].first, filename: "#{buddy['name']}.png", content_type: "image/png"),
+      email: Faker::Internet.free_email
+    )
+    puts "Created #{buddy['name']}"
+  end
+end
+puts 'Finished!'
