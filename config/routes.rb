7444a774afac2b30323c2b0d8393ae2e3c71a98b
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-21 21:36:23 +0100

    Routes and controllers - premier jet mais pull mal fait

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i[index show edit update] do
+    resources :prestations, exept: :destroy
+    resources :bookings, exept: :destroy
+  end
+  resources :prestations, only: :destroy
+  resources :bookings, only: :destroy
 end
