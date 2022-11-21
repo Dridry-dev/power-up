b54cc8b2a612d7f786063a4aa0f00f143d7fe119
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-21 22:12:59 +0100

    routes et controllers -all initiated but missing the client booking controller

--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -1,2 +1,24 @@
 class PrestationsController < ApplicationController
+  def index
+    @prestations = Prestation.all
+  end
+
+  def new
+    @prestation = Prestation.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
